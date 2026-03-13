17090451aaf8c7bd0de90bf4def3c0663f3f945a
Illia Khovanskyi <saskiuhia@gmail.com>
2026-03-13 15:29:50 +0100

    SearchEmbeddedHistoryInfo

--- JoyReactor.Accordion.Web/src/app/search/search-embedded-history-info/search-embedded-history-info.ts
+++ JoyReactor.Accordion.Web/src/app/search/search-embedded-history-info/search-embedded-history-info.ts
@@ -1,44 +1,29 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, inject, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { SearchEmbeddedHistoryRecord } from '../../../services/search-embedded-history-service/search-embedded-history-record';
-import { SearchEmbeddedType } from '../../../services/search-embedded-service/search-embedded-request';
+import { DatePipe } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { JoyReactorMediaMetadataService } from '../../../services/joyreactor-media-metadata-service/joyreactor-media-metadata-service';
 
 @Component({
   selector: 'app-search-embedded-history-info',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './search-embedded-history-info.html',
   styleUrl: './search-embedded-history-info.scss',
 })
 export class SearchEmbeddedHistoryInfo implements OnChanges {
+  private domSanitizer = inject(DomSanitizer);
+  joyReactorMediaMetadataService = inject(JoyReactorMediaMetadataService);
+
   @Input({ required: true }) historyRecord!: SearchEmbeddedHistoryRecord;
+
+  iframeUrl: SafeResourceUrl | null = null;
 
   ngOnChanges(changes: SimpleChanges): void {
     if (!this.historyRecord)
       return;
 
+    const iframeUrl = this.joyReactorMediaMetadataService.getIframeUrl(this.historyRecord);
+    this.iframeUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(iframeUrl);
     this.historyRecord.postIds = this.historyRecord.postIds.sort((a, b) => a - b);
-  }
-
-  getIFrameUrl(): string {
-    switch (this.historyRecord.type) {
-      case SearchEmbeddedType.BandCamp:
-        return ``;
-
-      case SearchEmbeddedType.Coub:
-        // <iframe src="//coub.com/embed/***?muted=false&autostart=false&originalSize=false&startWithHD=false" allowfullscreen frameborder="0" width="600" height="480" allow="autoplay"></iframe>
-        return `https://coub.com/embed/${this.historyRecord.text}`;
-
-      case SearchEmbeddedType.SoundCloud:
-        return ``;
-
-      case SearchEmbeddedType.Vimeo:
-        return ``;
-
-      case SearchEmbeddedType.YouTube:
-        // <iframe width="560" height="315" src="https://www.youtube.com/embed/***" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        return `https://www.youtube.com/embed/${this.historyRecord.text}`;
-
-      default:
-        return '';
-    }
   }
 }
--- JoyReactor.Accordion.Web/src/services/joyreactor-media-metadata-service/joyreactor-media-metadata-service.ts
+++ JoyReactor.Accordion.Web/src/services/joyreactor-media-metadata-service/joyreactor-media-metadata-service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { SearchEmbeddedHistoryRecord } from '../search-embedded-history-service/search-embedded-history-record';
+import { SearchEmbeddedType } from '../search-embedded-service/search-embedded-request';
 
 @Injectable({
   providedIn: 'root',
@@ -31,5 +33,34 @@ export class JoyReactorMediaMetadataService {
   createImageUrl(postAttributeId: number): string {
     // TODO: Add hostname ${hostName} instead of hardcoding joyreactor.cc
     return `https://img10.joyreactor.cc/pics/post/picture-${postAttributeId}.`;
+  }
+
+  getPostUrl(postId: number): string {
+    return `https://joyreactor.cc/${postId}`;
+  }
+
+  getIframeUrl(historyRecord: SearchEmbeddedHistoryRecord): string {
+    switch (historyRecord.type) {
+      case SearchEmbeddedType.BandCamp:
+        return ``;
+
+      case SearchEmbeddedType.Coub:
+        // <iframe src="//coub.com/embed/***?muted=false&autostart=false&originalSize=false&startWithHD=false" allowfullscreen frameborder="0" width="600" height="480" allow="autoplay"></iframe>
+        return `https://coub.com/embed/${historyRecord.text}`;
+
+      case SearchEmbeddedType.SoundCloud:
+        return ``;
+        
+      case SearchEmbeddedType.Vimeo:
+        // <iframe src="https://player.vimeo.com/video/***?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" referrerpolicy="strict-origin-when-cross-origin" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="AI-Powered Me"></iframe>
+        return `https://player.vimeo.com/video/${historyRecord.text}`;
+
+      case SearchEmbeddedType.YouTube:
+        // <iframe width="560" height="315" src="https://www.youtube.com/embed/***" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        return `https://www.youtube.com/embed/${historyRecord.text}`;
+
+      default:
+        return '';
+    }
   }
 }
