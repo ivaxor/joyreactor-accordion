926b5b93dffff94d9c84309865c1dfbe0585325a
Illia Khovanskyi <saskiuhia@gmail.com>
2026-05-02 04:47:36 +0200

    gtag

--- JoyReactor.Accordion.Web/src/app/app.config.ts
+++ JoyReactor.Accordion.Web/src/app/app.config.ts
@@ -4,16 +4,16 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { ConfigService } from '../services/config-service/config-service';
 import { HealthCheckService } from '../services/healthcheck-service/healthcheck-service';
-import { NgcCookieConsentConfig, provideNgcCookieConsent } from 'ngx-cookieconsent';
+import { NgcCookieConsentConfig, NgcCookieConsentService, provideNgcCookieConsent } from 'ngx-cookieconsent';
 
 const cookieConsentConfig: NgcCookieConsentConfig = {
   cookie: { domain: window.location.hostname },
-  position: 'top',
+  position: 'bottom-right',
   theme: 'block',
-  palette: { 
-    popup: { background: '#222222', text: '#fcfcfcbf' },
+  palette: {
+    popup: { background: '#626262', text: '#fcfcfc' },
     button: { background: '#fdb201', text: '#000000' },
-  },  
+  },
   type: 'opt-in',
   content: {
     message: 'Этот веб-сайт хочет использовать cookies чтобы следить за вашими действиями.',
@@ -21,13 +21,31 @@ const cookieConsentConfig: NgcCookieConsentConfig = {
     deny: 'Запретить',
     link: 'Google Analytics Terms of Service',
     href: 'https://marketingplatform.google.com/about/analytics/terms/',
-  }
+    policy: '🍪'
+  },
 };
 
 export const appConfig: ApplicationConfig = {
   providers: [
     { provide: LOCALE_ID, useValue: navigator.language },
     provideNgcCookieConsent(cookieConsentConfig),
+    provideAppInitializer(() => {
+      const gtagConsentUpdate = (status: string) => {
+        console.log(status);
+        const googleStatus = status === сookieConsentService.getStatus().allow! ? 'granted' : 'denied';
+        gtag('consent', 'update', {
+          'ad_storage': googleStatus,
+          'ad_user_data': googleStatus,
+          'ad_personalization': googleStatus,
+          'analytics_storage': googleStatus,
+        });
+      };
+
+      const сookieConsentService = inject(NgcCookieConsentService);
+      сookieConsentService.statusChange$.subscribe(event => gtagConsentUpdate(event.status));
+      if (сookieConsentService.hasConsented())
+        gtagConsentUpdate(сookieConsentService.getStatus().allow!);
+    }),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(),
