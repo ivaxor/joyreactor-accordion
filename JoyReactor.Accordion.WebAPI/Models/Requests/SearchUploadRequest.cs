87310cc463c82cabd4162c9032006a35f22d10cf
Illia Khovanskyi <saskiuhia@gmail.com>
2026-05-11 01:25:13 +0200

    SearchDownloadRequest & SearchUploadRequest

--- JoyReactor.Accordion.WebAPI/Models/Requests/SearchUploadRequest.cs
+++ JoyReactor.Accordion.WebAPI/Models/Requests/SearchUploadRequest.cs
@@ -9,12 +9,20 @@ public record SearchUploadRequest
     public IFormFile Media { get; set; }
 
     [Required]
+#if !DEBUG
     [Range(1, 5)]
+#else
+    [Range(1, 100)]
+#endif
     [DefaultValue(3)]
     public int Limit { get; set; }
 
     [Required]
-    [Range(0.8, 1)]
+#if !DEBUG
+    [Range(0.8, 1.0)]
+#else
+    [Range(0.0, 1.0)]
+#endif
     [DefaultValue(0.95f)]
     public float Threshold { get; set; }
 }
